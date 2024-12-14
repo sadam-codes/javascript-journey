64d23e658c360065b196bad1ddbb9b6b77a34d1e
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-14 18:47:25 +0500

    exception handling

---
+++ 25-ExceptionHandling.js
@@ -0,0 +1,34 @@
+try {
+  setTimeout(() => {
+    console.log("Hello");
+  }, 1000);
+} catch (error) {
+  error;
+}
+
+try {
+  setTimeout(() => {
+    console.log("Hacking wifi ..... Please wait....");
+    try {
+      console.log(sadam);
+    } catch (error) {
+      error;
+    }
+  }, 1000);
+} catch (error) {
+  error;
+}
+
+setTimeout(() => {
+  console.log("Fetching username and password ..... Please wait....");
+}, 2000);
+
+setTimeout(() => {
+  console.log("Hacking Sadam's Facebook id ....Please wait.... ");
+}, 3000);
+
+setTimeout(() => {
+  console.log(
+    "usename and password of sadam are  (+923044561544) fetched ... Please wait .... "
+  );
+}, 4000);
