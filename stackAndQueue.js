bbb3f83cfe78e998b23a68d0b03f5e42de358b0e
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-28 00:25:14 +0500

    stack and queue

---
+++ stackAndQueue.js
@@ -0,0 +1,25 @@
+const stack = [];
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+
+// pop
+stack.pop();
+stack.pop();
+console.log('using stack', stack);
+
+//queue
+
+const queue = [];
+queue.push(1);
+queue.push(2);
+queue.push(3);
+queue.push(4);
+queue.push(5);
+
+// pop
+queue.shift();
+queue.shift();
+console.log('using queue', queue);
