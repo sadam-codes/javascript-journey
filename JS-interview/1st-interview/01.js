32959b00fca29d1291421421dce9e69f4d15d4e8
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-27 22:57:41 +0500

    js interviews

---
+++ JS-interview/1st-interview/01.js
@@ -0,0 +1,27 @@
+// Q#01
+console.log(typeof NaN);
+// Q#02
+console.log(NaN === NaN);
+// Q#03
+console.log("5" + 3);
+console.log("5" - 3);
+// Q#04
+console.log("5" == 5);
+console.log("5" === 5);
+
+// Q#05
+// convert string to a number without variable
+console.log(+"12");
+console.log(typeof +"12"); //  number
+console.log(typeof +"Hi"); // number
+console.log(+"Hi"); // NAN
+
+// Q#06
+console.log(1 < 2 < 3); // true
+console.log(3 > 2 > 1); // false
+console.log(3 > 2 >= 1); // true
+// beacuse 1st two values compares firsty , if answer is true the it means its "1"
+// and then we cpmare 1 with next, not true
+
+
+// Q#07
---
+++ JS-interview/1st.js
@@ -0,0 +1,29 @@
+
+// convert string into number
+console.log(typeof +"12");
+//------------------------
+
+const names = [
+  {
+    first_name1: "sadam1",
+    second_name2: "sadam2",
+    second_name3: "sadam3",
+  },
+  {
+    first_name1: "sadam1",
+    2: "sadam2",
+    1: "sadam3",
+  },
+];
+
+console.log(names);
+console.log(Object.keys(names));
+console.log(Object.values(names));
+
+const name = {
+  first_name1: "sadam1",
+  2: "sadam2",
+  1: "sadam3",
+};
+
+console.log(Object.values(name));
