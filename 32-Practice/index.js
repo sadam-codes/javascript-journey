5950f3261f776a8be26e3d9e0e857ce942aefe3f
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-03-08 21:53:10 +0500

    practice questions

---
+++ 32-Practice/index.js
@@ -0,0 +1,82 @@
+const age = 22;
+const getYear = new Date().getFullYear();
+const result = (getYear - age)
+console.log(`Birth year is ${result}`)
+console.log(`Age is ${age}`)
+///////////////////////////////////
+
+// function sadam() {
+//     const x = 10
+// }
+// console.log(x)
+
+// output =>  ReferenceError: x is not defined
+//////////////////////////////////
+const number1 = 10;
+const number2 = 5
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+
+/////////////////////////////
+const stringOne = '123'
+console.log(typeof stringOne) //  Output: string
+// 
+const stringtwo = '123';
+console.log(typeof Number(stringtwo)); // Output: number
+////////////////////////////
+
+let number = 8;
+if (number % 2 === 0) {
+    console.log("Number is even")
+}
+else {
+    console.log("Number is Odd")
+}
+//////////////////////////
+const person = 12
+if (person >= 20) {
+    console.log("Person is Senior")
+}
+else if (person <= 19 && person >= 15) {
+    console.log("Person is Adult");
+}
+else if (person <= 14 && person >= 12) {
+    console.log("Person is teenager")
+}
+else {
+    console.log("Person is child")
+}
+///////////////////////////
+const checkNumber = 10;
+const resultAfterChecking = (checkNumber > 0) ? "Number is Positive" : (checkNumber < 0) ? "Number is  Negative" : "Number is Zero"
+console.log(resultAfterChecking)
+/////////////////////////
+const trafficLight = "Go";
+switch (trafficLight) {
+    case "Go":
+        console.log("Green Light - You can go!");
+        break;
+    case "Ready":
+        console.log("Yellow Light - Get Ready!");
+        break;
+    case "Stop":
+        console.log("Red Light - Stop!");
+        break;
+    default:
+        console.log("Invalid traffic light signal");
+}
+///////////////////////////////
+
+for (let index = 1; index <= 10; index++) {
+    console.log(index)
+}
+
+/////////////////////////////
+const stop = null
+while (stop === stop) {
+    stop(prompt("enter word stop to end this loop"))
+    console.log(stop)
+}
+console.log("Loop Stoped")
