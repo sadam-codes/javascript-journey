882ce24db43cee9c5365677651f1576b5ec16ffe
sadam-codes <sadam.fullstackdev@gmail.com>
2025-05-10 20:21:21 +0500

    Update 1st.js

--- JS-interview/1st.js
+++ JS-interview/1st.js
@@ -5,14 +5,14 @@ console.log(typeof +"12");
 
 const names = [
   {
-    first_name1: "sadam1",
-    second_name2: "sadam2",
-    second_name3: "sadam3",
+    first_name1: "Sadam1",
+    second_name2: "Sadam2",
+    second_name3: "Sadam3",
   },
   {
-    first_name1: "sadam1",
-    2: "sadam2",
-    1: "sadam3",
+    first_name1: "Sadam1",
+    2: "Sadam2",
+    1: "Sadam3",
   },
 ];
 
@@ -21,13 +21,13 @@ console.log(Object.keys(names));
 console.log(Object.values(names));
 
 const name = {
-  first_name1: "sadam1",
-  2: "sadam2",
-  1: "sadam3",
+  first_name1: "Sadam1",
+  2: "Sadam2",
+  1: "Sadam3",
 };
 
 console.log(Object.values(name));
 
 
-const myVar = 5;
+const myVar = 10;
 console.log(myVar); 
